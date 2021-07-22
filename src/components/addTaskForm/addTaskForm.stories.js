747b7c89bdffd807da2f3ed60f0a3b25604ad406
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-07-22 15:31:56 +0200

    Add basic file structure / stories setup

---
+++ src/components/addTaskForm/addTaskForm.stories.js
@@ -0,0 +1,9 @@
+import "./addTaskForm.css";
+import addTaskForm from "./addTaskForm.html";
+
+export default {
+  title: "Components/Form",
+  parameters: { layout: "fullscreen" },
+};
+
+export const taskForm = () => addTaskForm;
