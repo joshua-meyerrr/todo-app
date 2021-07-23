d9bb4ba0e39b23d6f2d6ad08758d4658c33df553
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-07-23 19:58:18 +0200

    Add basic file-structure / stories setup

---
+++ src/components/burgerMenu/burgerMenu.stories.js
@@ -0,0 +1,9 @@
+import "./burgerMenu.css";
+import burgerMenu from "./burgerMenu.html";
+
+export default {
+  title: "Components/Button",
+  parameters: { layout: "fullscreen" },
+};
+
+export const burger = () => burgerMenu;
