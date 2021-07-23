6f3c874d93cc4d04a653ecd475167413dd9ab333
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-07-23 16:05:06 +0200

    Add basic file-structure

---
+++ src/components/taskList/taskList.stories.js
@@ -0,0 +1,9 @@
+import "./taskList.css";
+import taskList from "./taskList.html";
+
+export default {
+  title: "Components/List",
+  parameters: { layout: "fullscreen" },
+};
+
+export const taskUl = () => taskList;
