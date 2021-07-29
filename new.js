cdbe0f0ec0edc2c811864ffa59cbf0c265be5aa6
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-07-29 10:49:03 +0200

    Add new js functionality to save entered data

--- new.js
+++ new.js
@@ -1,5 +1,3 @@
-// alt button
-
 const altBtnEl = document.querySelector(".addTaskBtn--alternative");
 
 altBtnEl.onclick = alertSelectedDate;
@@ -14,3 +12,22 @@ function alertSelectedDate() {
 const submitBtnEl = document.querySelector(".add-task-btn");
 
 const formEl = document.querySelector(".task-form");
+
+formEl.onsubmit = function (event) {
+  // Prevent the default functionality of the submit event, which is reloading the page
+  event.preventDefault();
+
+  const newTask = { title: "", date: "", isDone: false };
+
+  const checkedDateInput = document.querySelector(".radio__input:checked");
+  const textInput = document.querySelector(".task-form__textInput");
+
+  newTask.title = textInput.value;
+  newTask.date = checkedDateInput.value;
+
+  // template literals
+  console.log(
+    `Hi you entered "${newTask.date}" as date and "${newTask.title}" as description`
+  );
+  console.log(newTask);
+};
