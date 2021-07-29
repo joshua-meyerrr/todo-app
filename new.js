0fcaf5edde5da17c31597cef232dec69f0f57cc1
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-07-29 14:57:51 +0200

    Go Macel

--- new.js
+++ new.js
@@ -5,11 +5,8 @@ altBtnEl.onclick = alertSelectedDate;
 function alertSelectedDate() {
   const checkedInput = document.querySelector(".radio__input:checked");
 
-  console.log(checkedInput.value);
+  alert(checkedInput.value);
 }
-
-// submit button
-const submitBtnEl = document.querySelector(".add-task-btn");
 
 const formEl = document.querySelector(".task-form");
 
